2bcb36644d2fc824f5ce5f686cf90f5464f7a2aa
nitindatta <nitin.datta@gmail.com>
2020-10-02 19:35:21 +0530

    db done

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
  mkdir ./deploy/publish
  dotnet publish ./src -c Release -o ./deploy/publish
  mkdir ./deploy/sampledotnet-deploy
- zip a  ./deploy/sampledotnet-deploy/sampledotnet.zip  ./deploy/publish/*.*
+ zip a  ./deploy/sampledotnet-deploy/sampledotnet.zip  ./deploy/publish/*
  cp ./src/aws-windows-deployment-manifest.json ./deploy/sampledotnet-deploy
  zip a  ./deploy/sampledotnet-deploy.zip  ./deploy/sampledotnet-deploy/*.*
  rm -r -f ./deploy/publish
