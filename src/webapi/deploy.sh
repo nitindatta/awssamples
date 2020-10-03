15def97af95f9038ac0ba0083f63626f054d8ef2
nitindatta <nitin.datta@gmail.com>
2020-10-03 21:59:52 +0530

    restructuring

--- src/webapi/deploy.sh
+++ src/webapi/deploy.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 
- rm -r -f ./deploy/publish
+ rm -r -f ./deploy
+ mkdir ./deploy/
  mkdir ./deploy/publish
- dotnet publish ./src -c Release -o ./deploy/publish
+ dotnet publish . -c Release -o ./deploy/publish
  mkdir ./deploy/sampledotnet-deploy
  zip a  ./deploy/sampledotnet-deploy/sampledotnet.zip  ./deploy/publish/*
- cp ./src/aws-windows-deployment-manifest.json ./deploy/sampledotnet-deploy
+ mv ./aws-windows-deployment-manifest.json ./deploy/sampledotnet-deploy
  zip a  ./deploy/sampledotnet-deploy.zip  ./deploy/sampledotnet-deploy/*.*
- rm -r -f ./deploy/publish
- rm -r -f ./deploy/sampledotnet-deploy
+ #rm -r -f ./deploy
